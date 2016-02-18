c98a9239e124e9e0d3b4f55df0e993f197ac088b
Sergey Beresnev <s.beresnev@mfc38.ru>
2016-02-18 10:33:32 +0800

    Migration generated from entities
    $ php bin/console doctrine:migrations:diff

---
+++ app/DoctrineMigrations/Version20160218033323.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160218033323 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, question_category_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX question_category_id (question_category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE question_category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494EF142426F FOREIGN KEY (question_category_id) REFERENCES question_category (id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494EF142426F');
+        $this->addSql('DROP TABLE question');
+        $this->addSql('DROP TABLE question_category');
+    }
+}
